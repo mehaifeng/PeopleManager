c1ac955a36f781b0a5a02df6b051f9bd09628393
HF_A <aihaifeng008@outlook.com>
2023-04-22 19:51:25 +0800

    normal develop update

---
+++ DoctorWorkDayManager/Views/SchedulingView/SchedulingView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoctorWorkDayManager.Views.SchedulingView
+{
+    /// <summary>
+    /// SchedulingView.xaml 的交互逻辑
+    /// </summary>
+    public partial class SchedulingView : UserControl
+    {
+        public SchedulingView()
+        {
+            InitializeComponent();
+            Department_combobox.ItemsSource = new List<string>
+            {
+                "全部",
+                "内科",
+                "外科",
+                "骨科",
+                "眼科",
+                "泌尿科",
+                "妇科",
+                "口腔科",
+                "消化内科"
+            };
+            Department_combobox.SelectedValue = "全部";
+        }
+    }
+}
